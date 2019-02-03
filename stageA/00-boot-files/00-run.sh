b26848d1e077390b9f943079939cd90bd1d7dae8
mstroh <m.stroh@ymail.com>
2019-02-03 22:42:12 +0100

    remove newline before append

--- stageA/00-boot-files/00-run.sh
+++ stageA/00-boot-files/00-run.sh
@@ -4,7 +4,7 @@ rm -rf files/git
 git clone https://github.com/GrazerComputerClub/rpi-boot files/git
 install -m 644 files/git/*.txt ${ROOTFS_DIR}/boot/
 install -m 644 files/git/overlays/*.dtbo ${ROOTFS_DIR}/boot/overlays/
-sed -i -e 's/\n/ /g' ${ROOTFS_DIR}/boot/cmdline.txt
+perl -i -p -e 's/\n/ /' ${ROOTFS_DIR}/boot/cmdline.txt
 cat files/append_cmdline.txt >> ${ROOTFS_DIR}/boot/cmdline.txt
 cat files/append_config.txt >> ${ROOTFS_DIR}/boot/config.txt
 cat files/append_dhcpcd.conf >> ${ROOTFS_DIR}/etc/dhcpcd.conf
