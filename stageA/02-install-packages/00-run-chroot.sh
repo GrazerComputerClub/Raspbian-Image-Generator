021edc0ca599977a8bf40860f2966ea2868cfb2f
m.stroh76 <m.stroh76@gmail.com>
2019-11-10 15:05:42 +0100

    ignore lirc install error

--- stageA/02-install-packages/00-run-chroot.sh
+++ stageA/02-install-packages/00-run-chroot.sh
@@ -5,7 +5,7 @@ pip3 install esptool websockets wiringpi adafruit-circuitpython-am2320 Adafruit_
 pip install wiringpi -t /usr/local/lib/python2.7/dist-packages/
 
 apt-get install /home/pi/wiringpi-latest.deb
-apt-get install /home/pi/liblirc0_0.10.1-5.2_armhf.deb /home/pi/liblirc-client0_0.10.1-5.2_armhf.deb /home/pi/lirc_0.10.1-5.2_armhf.deb
+apt-get install -y --allow-downgrades /home/pi/liblirc0_0.10.1-5.2_armhf.deb /home/pi/liblirc-client0_0.10.1-5.2_armhf.deb /home/pi/lirc_0.10.1-5.2_armhf.deb || echo irgnore error no /dev/lirc
 apt-mark hold liblirc0 liblirc-client0 lirc
 rm /home/pi/*.deb
 
