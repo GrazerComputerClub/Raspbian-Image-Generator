e821d7630cfe5f0ac930d29f0441895201a69a92
m.stroh76 <m.stroh76@gmail.com>
2019-11-10 12:26:36 +0100

    patched lirc and IR-receiver overlay

--- stageA/02-install-packages/00-run-chroot.sh
+++ stageA/02-install-packages/00-run-chroot.sh
@@ -5,6 +5,8 @@ pip3 install esptool websockets wiringpi adafruit-circuitpython-am2320 Adafruit_
 pip install wiringpi -t /usr/local/lib/python2.7/dist-packages/
 
 apt-get install /home/pi/wiringpi-latest.deb
+apt-get install /home/pi/liblirc0_0.10.1-5.2_armhf.deb /home/pi/liblirc-client0_0.10.1-5.2_armhf.deb /home/pi/lirc_0.10.1-5.2_armhf.deb
+apt-mark hold liblirc0 liblirc-client0 lirc
 rm /home/pi/*.deb
 
 
