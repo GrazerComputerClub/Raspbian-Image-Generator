01f5c2c97ea433ce7376092cd53416020d24eb7c
mstroh <m.stroh@ymail.com>
2020-03-22 17:52:30 +0100

    bug fix: kernel install & ACT-LED mmc0

--- stageA/00-boot-bpi-files/00-run-chroot.sh
+++ stageA/00-boot-bpi-files/00-run-chroot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-apt-get install /home/pi/linux-image-4.19.12-m2z-otg-gadget_1.0-121_armhf.deb
+apt-get install /home/pi/linux-image-4.19.12-m2z-otg-gadget_1.0-221_armhf.deb
 
 rm /home/pi/linux-image-*.deb
 
