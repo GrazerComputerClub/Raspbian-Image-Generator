1c284b337d824b68de4fa7d30d5e8887ccf172a8
mstroh <m.stroh@ymail.com>
2020-03-30 21:39:44 +0200

    Banana Pi M2 Zero new Kernel

--- stageA/00-boot-bpi-files/00-run.sh
+++ stageA/00-boot-bpi-files/00-run.sh
@@ -3,6 +3,10 @@
 git clone https://github.com/GrazerComputerClub/Banana-Pi-M2-Zero.git files/Banana-Pi-M2-Zero
 cp -v files/Banana-Pi-M2-Zero/kernel/linux-image-*.deb "${ROOTFS_DIR}/home/pi/"
 cp -v -r -b files/Banana-Pi-M2-Zero/firmware/* "${ROOTFS_DIR}/lib/firmware/"
-cp -v -r files/Banana-Pi-M2-Zero/boot/* "${ROOTFS_DIR}/boot/"
+cp -v -r files/Banana-Pi-M2-Zero/boot/boot.scr "${ROOTFS_DIR}/boot/"
+cp -v -r files/Banana-Pi-M2-Zero/boot/bpi-m2-zero_boot.cmd "${ROOTFS_DIR}/boot/"
 rm -rf files/Banana-Pi-M2-Zero
+
+# support only 115200 baudrate : http://0pointer.de/blog/projects/serial-console.html
+sed -i -e 's/--keep-baud 115200,38400,9600/--keep-baud 115200/g' ${ROOTFS_DIR}/usr/lib/systemd/system/serial-getty@.service
 
