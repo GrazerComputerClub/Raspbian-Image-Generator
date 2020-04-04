b72eba768e60106ee9dd5edd2e9b0be0b1c1be1c
mstroh <m.stroh@ymail.com>
2020-04-04 17:18:55 +0200

    wrong prameter

--- stageB/00-install-packages/01-run.sh
+++ stageB/00-install-packages/01-run.sh
@@ -7,5 +7,5 @@ update-alternatives --install /usr/bin/gnome-www-browser \
   gnome-www-browser /usr/bin/chromium-browser 86
 EOF
 
-cp files/GC2_Logo.png -O ${ROOTFS_DIR}/usr/share/lxde/images/GC2.png
+cp files/GC2_Logo.png ${ROOTFS_DIR}/usr/share/lxde/images/GC2.png
 cp -rf files/config/* ${ROOTFS_DIR}/home/pi/.config/
