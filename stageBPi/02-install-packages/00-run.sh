67a8d1f64e5dfabb85f3a4c377af6f64e894e815
mstroh <m.stroh@ymail.com>
2020-05-10 20:19:58 +0200

    create board.sh for bananapi m2z detection

--- stageBPi/02-install-packages/00-run.sh
+++ stageBPi/02-install-packages/00-run.sh
@@ -4,4 +4,4 @@ WWW_USER=33
 PI_USER=1000
 
 wget https://github.com/GrazerComputerClub/WiringPi/releases/download/2.60.200413/wiringpi-2.60.200413-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/wiringpi-latest.deb"
-
+install files/board.sh ${ROOTFS_DIR}/var/lib/bananapi/
---
+++ stageBPi/02-install-packages/files/board.sh
@@ -0,0 +1,1 @@
+BOARD=bpi-m2z
