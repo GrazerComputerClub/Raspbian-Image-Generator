58a02186c3131e7928d654a8d426f5c878ca1947
mstroh <m.stroh@ymail.com>
2020-05-10 21:18:17 +0200

    RPi.GPIO install

--- stageBPi/02-install-packages/00-run-chroot.sh
+++ stageBPi/02-install-packages/00-run-chroot.sh
@@ -2,7 +2,10 @@
 
 
 apt-get install /home/pi/wiringpi-latest.deb
-rm /home/pi/wiringpi-latest.deb
+apt-get install /home/pi/RPi.GPIO-py3-latest.deb
+apt-get install /home/pi/RPi.GPIO-py2-latest.deb
+
+rm /home/pi/*.deb
 
 
 
--- stageBPi/02-install-packages/00-run.sh
+++ stageBPi/02-install-packages/00-run.sh
@@ -4,4 +4,9 @@ WWW_USER=33
 PI_USER=1000
 
 wget https://github.com/GrazerComputerClub/WiringPi/releases/download/2.60.200413/wiringpi-2.60.200413-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/wiringpi-latest.deb"
+wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/v0.7.0-1/python3-rpi.gpio_0.7.0.buster-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/RPi.GPIO-py3-latest.deb"
+wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/v0.7.0-1/python-rpi.gpio_0.7.0~buster-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/RPi.GPIO-py2-latest.deb"
+
 install files/board.sh ${ROOTFS_DIR}/var/lib/bananapi/
+
+
