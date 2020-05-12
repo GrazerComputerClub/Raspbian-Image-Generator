25f3438b202a3a2d8b9e905fc951ed14fbe61b79
mstroh76 <m.stroh76@gmail.com>
2020-05-12 15:08:28 +0200

    not installing RPi.GPIO

--- stageBPi/00-boot-files/00-run-chroot.sh
+++ stageBPi/00-boot-files/00-run-chroot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-apt-get install /home/pi/linux-image-4.19.12-m2z-otg-gadget_1.0-321_armhf.deb
+apt-get install /home/pi/linux-image-4.19.12-m2z-otg-gadget_1.0-421_armhf.deb
 
-rm /home/pi/linux-image-*.deb
+rm -v /home/pi/linux-image-*.deb
 
 # Debug UART enable
-#systemctl enable serial-getty@ttyS2.service
+systemctl enable serial-getty@ttyS2.service
 
 
 
--- stageBPi/02-install-packages/00-run-chroot.sh
+++ stageBPi/02-install-packages/00-run-chroot.sh
@@ -2,10 +2,11 @@
 
 
 apt-get install /home/pi/wiringpi-latest.deb
-apt-get install /home/pi/RPi.GPIO-py3-latest.deb
-apt-get install /home/pi/RPi.GPIO-py2-latest.deb
+#apt-get install /home/pi/RPi.GPIO-py3-latest.deb
+#apt-get install /home/pi/RPi.GPIO-py2-latest.deb
 
-rm /home/pi/*.deb
+#rm /home/pi/*.deb
+rm -v /home/pi/wiringpi*.deb
 
 
 
