08bbb441f59e2c8ea32f1e8ca5397a01b6dc816f
mstroh <m.stroh@ymail.com>
2020-05-15 18:07:45 +0200

    RPi.GPIO install

--- stageBPi/02-install-packages/00-run-chroot.sh
+++ stageBPi/02-install-packages/00-run-chroot.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
 
-
 apt-get install /home/pi/wiringpi-latest.deb
-#apt-get install -y --allow-downgrades /home/pi/RPi.GPIO-py3-latest.deb
-#apt-get install -y --allow-downgrades /home/pi/RPi.GPIO-py2-latest.deb
-#apt-mark hold python-rpi.gpio python3-rpi.gpio
+apt-get install -y --allow-downgrades /home/pi/RPi.GPIO-py3-latest.deb
+apt-get install -y --allow-downgrades /home/pi/RPi.GPIO-py2-latest.deb
+apt-mark hold python-rpi.gpio python3-rpi.gpio 
+patch /usr/lib/python3/dist-packages/gpiozero/pins/data.py /home/pi/gpiozero.patch
+apt-mark hold python3-gpiozero
 
-#rm /home/pi/*.deb
-rm -v /home/pi/wiringpi*.deb
+rm -v /home/pi/wiringpi*.deb /home/pi/RPi.GPIO*.deb /home/pi/gpiozero.patch
 
+# BananaPi not supported 
+sudo apt -y remove pigpio libpigpio-dev libpigpio1 libpigpiod-if-dev libpigpiod-if1 libpigpiod-if2-1 pigpio-tools pigpiod
 
 
--- stageBPi/02-install-packages/00-run.sh
+++ stageBPi/02-install-packages/00-run.sh
@@ -4,8 +4,9 @@ WWW_USER=33
 PI_USER=1000
 
 wget https://github.com/GrazerComputerClub/WiringPi/releases/download/2.60.200413/wiringpi-2.60.200413-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/wiringpi-latest.deb"
-wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/v0.7.0-1/python3-rpi.gpio_0.7.0.buster-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/RPi.GPIO-py3-latest.deb"
-wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/v0.7.0-1/python-rpi.gpio_0.7.0.buster-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/RPi.GPIO-py2-latest.deb"
+wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/v0.7.0-buster1/python-rpi.gpio_0.7.0.buster-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/RPi.GPIO-py3-latest.deb"
+wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/v0.7.0-buster1/python3-rpi.gpio_0.7.0.buster-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/RPi.GPIO-py2-latest.deb"
+wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/v0.7.0-buster1/gpiozero.patch -O "${ROOTFS_DIR}/home/pi/gpiozero.patch"
 
 install -v -o ${PI_USER} -g ${PI_USER} -m 755 -d "${ROOTFS_DIR}/var/lib/bananapi/"
 install -v -o ${PI_USER} -g ${PI_USER} -m 744 files/board.sh ${ROOTFS_DIR}/var/lib/bananapi/
