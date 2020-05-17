6a5d400eaef650f3460912adc404493968e848d9
mstroh <m.stroh@ymail.com>
2020-05-17 18:19:27 +0200

    Blockly-gPIo server run as root for BananaPi

--- stageBPi/03-install-services/00-run-chroot.sh
+++ stageBPi/03-install-services/00-run-chroot.sh
@@ -3,3 +3,8 @@
 # dmxvnc only works with raspberrypi GPU
 systemctl disable dmxvnc
 
+# Blockly-gPIo needs root to access gpios with Banana Pi kernel 
+sed -i 's/^User=.*/User=root/g' /etc/systemd/system/blockly-gpio.service
+
+systemctl daemon-reload
+
