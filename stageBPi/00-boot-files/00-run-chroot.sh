16c08d3b5b981c75031bdf2ba03eb3d3d1a26e66
mstroh <m.stroh@ymail.com>
2020-06-20 11:02:42 +0200

    BPi: new kernel, bin, patch and libs

--- stageBPi/00-boot-files/00-run-chroot.sh
+++ stageBPi/00-boot-files/00-run-chroot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-apt-get install /home/pi/linux-image-4.19.12-m2z-otg-gadget_1.0-421_armhf.deb
+apt-get install /home/pi/linux-image-4.19.12-m2z-otg-gadget_1.0-521_armhf.deb
 
 rm -v /home/pi/linux-image-*.deb
 
-# Debug UART enable
+# Debug UART GPIO enable
 systemctl enable serial-getty@ttyS2.service
 
 
--- stageBPi/02-install-packages/00-run.sh
+++ stageBPi/02-install-packages/00-run.sh
@@ -3,15 +3,28 @@
 WWW_USER=33
 PI_USER=1000
 
+# copy patches
+git clone https://github.com/GrazerComputerClub/Banana-Pi-M2-Zero.git files/Banana-Pi-M2-Zero
+mv files/Banana-Pi-M2-Zero/patch/*.diff 01-patches
+ls 01-patches | grep .diff > 01-patches/series
+
+# revert old changes for avrdude (needed for patch)
+cp -fv "${ROOTFS_DIR}/etc/avrdude.conf.orig" "${ROOTFS_DIR}/etc/avrdude.conf"
+
+#install replace bin
+cp -fv files/Banana-Pi-M2-Zero/bin/* "${ROOTFS_DIR}/usr/local/bin"
+
+
+# install GC2 version of gpio librarys
 wget https://github.com/GrazerComputerClub/WiringPi/releases/download/2.60.200613/wiringpi-2.60.200613-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/wiringpi-latest.deb"
-wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/v0.7.0-buster1/python-rpi.gpio_0.7.0.buster-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/RPi.GPIO-py3-latest.deb"
-wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/v0.7.0-buster1/python3-rpi.gpio_0.7.0.buster-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/RPi.GPIO-py2-latest.deb"
+wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/0.7.200620-2/python3-rpi.gpio_0.7.200620.buster-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/RPi.GPIO-py3-latest.deb"
+wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/0.7.200620-2/python3-rpi.gpio_0.7.200620.buster-1_armhf.deb -O "${ROOTFS_DIR}/home/pi/RPi.GPIO-py2-latest.deb"
 wget https://github.com/GrazerComputerClub/RPi.GPIO/releases/download/v0.7.0-buster1/gpiozero.patch -O "${ROOTFS_DIR}/home/pi/gpiozero.patch"
 
-wget https://github.com/GrazerComputerClub/Raspjamming-OS_Banana-Pi-M2-Zero/raw/master/patch/avrdude.diff -P 01-patches
-cp -f "${ROOTFS_DIR}/etc/avrdude.conf.orig" "${ROOTFS_DIR}/etc/avrdude.conf"
-
+## old bananapi recognition for old librarys
 install -v -o ${PI_USER} -g ${PI_USER} -m 755 -d "${ROOTFS_DIR}/var/lib/bananapi/"
 install -v -o ${PI_USER} -g ${PI_USER} -m 744 files/board.sh ${ROOTFS_DIR}/var/lib/bananapi/
 
+
+rm -rf files/Banana-Pi-M2-Zero
 
