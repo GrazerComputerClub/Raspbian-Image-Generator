ad0deb7fd2837a1f217f7442b081c604c982b7d2
mstroh <m.stroh@ymail.com>
2020-07-02 19:05:10 +0200

    new kernel

--- stageBPi/00-boot-files/00-run-chroot.sh
+++ stageBPi/00-boot-files/00-run-chroot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 apt-get install /home/pi/linux-image-4.19.12-m2z-otg-gadget_1.0-521_armhf.deb
+cp /boot/zImage /boot/zImage-4.19.12-m2z-otg-gadget
+apt-get install /home/pi/linux-image-4.19.129-sunxi_2.0-100_armhf.deb
 
 rm -v /home/pi/linux-image-*.deb
 
