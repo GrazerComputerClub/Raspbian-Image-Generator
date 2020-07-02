0ce6cfa55dc842c81eec1ac07107d95acd4b68fe
mstroh <m.stroh@ymail.com>
2020-07-02 19:06:00 +0200

    set beta version for BPi

--- stageBPi/00-boot-files/00-run.sh
+++ stageBPi/00-boot-files/00-run.sh
@@ -7,6 +7,8 @@ cp -v -r files/Banana-Pi-M2-Zero/boot/boot.scr "${ROOTFS_DIR}/boot/"
 cp -v -r files/Banana-Pi-M2-Zero/boot/bpi-m2-zero_boot.cmd "${ROOTFS_DIR}/boot/"
 rm -rf files/Banana-Pi-M2-Zero
 
+echo "Beta" >> ${ROOTFS_DIR}/etc/raspjamming_version
+
 # support only 115200 baudrate : http://0pointer.de/blog/projects/serial-console.html
 sed -i -e 's/--keep-baud 115200,38400,9600/--keep-baud 115200/g' ${ROOTFS_DIR}/usr/lib/systemd/system/serial-getty@.service
 
