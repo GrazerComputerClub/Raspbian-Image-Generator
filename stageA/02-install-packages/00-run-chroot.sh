eb257bf7dbf5d750c32a434fd2ec7dffa956243b
mstroh76 <m.stroh76@gmail.com>
2021-02-14 23:11:34 +0100

    micropython tools

--- stageA/02-install-packages/00-run-chroot.sh
+++ stageA/02-install-packages/00-run-chroot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-pip3 install esptool websockets wiringpi adafruit-circuitpython-am2320 Adafruit_DHT -t /usr/local/lib/python3.5/dist-packages
-pip3 install esptool websockets wiringpi adafruit-circuitpython-am2320 Adafruit_DHT -t /usr/local/lib/python3.7/dist-packages
-pip install wiringpi -t /usr/local/lib/python2.7/dist-packages/
+pip3 install esptool websockets wiringpi adafruit-circuitpython-am2320 Adafruit_DHT rshell -t /usr/local/lib/python3.5/dist-packages
+pip3 install esptool websockets wiringpi adafruit-circuitpython-am2320 Adafruit_DHT rshell -t /usr/local/lib/python3.7/dist-packages
+pip install wiringpi adafruit-ampy -t /usr/local/lib/python2.7/dist-packages/
 
 apt-get install /home/pi/wiringpi-latest.deb
 apt-get install -y --allow-downgrades /home/pi/liblirc0_0.10.1-5.2_armhf.deb /home/pi/liblirc-client0_0.10.1-5.2_armhf.deb /home/pi/lirc_0.10.1-5.2_armhf.deb || echo ignoring error no /dev/lirc
